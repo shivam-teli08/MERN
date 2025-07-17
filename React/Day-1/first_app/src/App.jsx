bf0af635de081f91a8a4b7bdc92269acee706bef
shivamteli-08 <shivam.teli.cs@gmail.com>
2025-07-17 16:40:50 +0530

    Day-1

---
+++ React/Day-1/first_app/src/App.jsx
@@ -0,0 +1,13 @@
+//
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="my-name">Hello This is First app created by Shivam</h1>
+    </>
+  )
+}
+
+export default App
